da2efe4e792f13362de69ad783ca8468ed5a8d5a
Brett Wise <brettdwise@gmail.com>
2014-10-20 17:00:40 -0400

    structured sessions to allow temporary and permanent logins using cookies. implemented a checkbox to allow perm logins. implemented logic to change links and layout based on login status. used flash message for login messages. added a remember token and corresponding remember digest for use in persistent sessions. learned that tenary operators were a compact way to write simple if-then statements. added integration tests for all the new features.

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -16,16 +16,18 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select 'div.alert.alert-danger', 1
   end
 
-    test "valid signup information" do
-      get signup_path
-      name      = "Example User"
-      email     = "user@example.com"
-      password  = "password"
-      assert_difference 'User.count', 1 do
-        post_via_redirect users_path, user: { name: name, 
-                                              email: email, 
-                                              password:              password,
-                                              password_confirmation: password}
-      end
-    end 
+  test "valid signup information" do
+    get signup_path
+    name      = "Example User"
+    email     = "user@example.com"
+    password  = "password"
+    assert_difference 'User.count', 1 do
+      post_via_redirect users_path, user: { name: name, 
+                                            email: email, 
+                                            password:              password,
+                                            password_confirmation: password}
+    end
+    assert_template 'users/show'
+    assert is_logged_in?
+  end 
 end
